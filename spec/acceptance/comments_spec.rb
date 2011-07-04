29957dba0750eb16f149d9c6f15245153b2d81b5
Danilo Moret <moret@corp.globo.com>
2011-07-04 13:27:52 -0300

    Created add comments

--- spec/acceptance/comments_spec.rb
+++ spec/acceptance/comments_spec.rb
@@ -9,12 +9,12 @@ describe "Comments" do
     it "should succed" do
       visit post_path(@post)
 
-      fill_in "Comment", :with => "Post rulez!"
-      click_link "Create comment"
+      fill_in "My comment", :with => "Post rulez!"
+      click_button "Create Comment"
 
       page.should have_content("Your comment was successfully added to the post.")
       page.should have_content("Post rulez!")
-      post.current_path.should == post_patch(@post)
+      page.current_path.should == post_path(@post)
     end
   end
 end

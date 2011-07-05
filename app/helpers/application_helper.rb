030e49dbb9636c904ad29bf7e4cd376b7712d320
Danilo Moret <moret@corp.globo.com>
2011-07-05 10:27:40 -0300

    Refactoring flash_messages to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  FLASH_KEYS = [:notice, :alert]
+
+  def flash_messages
+    content = "".html_safe
+    FLASH_KEYS.each do |key|
+      value = flash[key]
+      next if value.blank?
+      content << content_tag(:p, value, :id => "flash_#{key}")
+    end
+    content
+  end
 end

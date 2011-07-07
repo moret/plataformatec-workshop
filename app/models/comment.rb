8c7a7dbb2bd0d937757c2d49812bd7f3a4af8fa6
Danilo Moret <github@moret.pro.br>
2011-07-07 10:13:48 -0300

    Sending email to author when post gets comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   validates_presence_of :body
+
+  before_create :send_post_to_author
+
+  private
+
+  def send_post_to_author
+    CommentsNotifier.to_post_author(self).deliver
+  end
 end
